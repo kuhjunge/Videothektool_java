907ce633b6603960c384b8bd908affe7cabe1a33
Kuhjunge <quhberta@gmail.com>
2013-11-30 11:52:47 +0100

    - Externe Jars eingebunden

--- VideothekTool/src/AddMovieDialog.java
+++ VideothekTool/src/AddMovieDialog.java
@@ -1,7 +1,6 @@
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.List;
 import java.util.Map;
 
 import javax.swing.JButton;
