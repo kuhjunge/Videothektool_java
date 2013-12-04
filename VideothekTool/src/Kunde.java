5d036d4944e4b3d0bc61174dba145c7ef34a253f
jhary82 <jhary82@gmx.de>
2013-12-04 19:27:34 +0100

    Hinzufügen der Klasse Kunde

---
+++ VideothekTool/src/Kunde.java
@@ -0,0 +1,38 @@
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+/**
+ * @author Simon Krause
+ * @version 1.0
+ *
+ */
+@DatabaseTable(tableName = "Kunde")
+public class Kunde {
+	@DatabaseField(id = true)
+	private int idKunde;
+	@DatabaseField
+	private String vorname;
+	@DatabaseField
+	private String nachname;
+	@DatabaseField
+	private String strasse;
+	@DatabaseField
+	private String hausnr;
+	@DatabaseField
+	private String plz;
+	@DatabaseField
+	private String stadt;
+	@DatabaseField
+	private String gebDatum;
+	@DatabaseField
+	private String telNr;
+	@DatabaseField
+	private String email;
+	
+	/**
+	 * Default Konstruktor
+	 */
+	public Kunde(){
+		
+	}
+}
